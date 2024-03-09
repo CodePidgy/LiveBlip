d532d09f5787375321fabbfec451cb6519fa6897
Aidan <aidanbv03@gmail.com>
2024-03-10 00:00:12 +0200

    Add testing on local server

--- TrackerService/Program.cs
+++ TrackerService/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using Namespace;
 
 namespace TrackerService;
 
 public class Program {
-
 	public static void Main(string[] args) {
+		bool test = true;
+
 		int port = 22368;
 
 		TcpListener listener = new(IPAddress.Any, port);
@@ -16,6 +19,10 @@ public class Program {
 
 		Console.WriteLine($"Listening for TCP connections on port {port}...");
 
+		if (test) {
+			new Thread(Test).Start();
+		}
+
 		while (true) {
 			// Accept a new client when they attempt to connect
 			TcpClient client = listener.AcceptTcpClient();
@@ -23,5 +30,23 @@ public class Program {
 			// Create a new client object to handle the client's connection
 			_ = new Client(client);
 		}
+	}
+
+	// methods ---------------------------------------------------------------------------------- //
+	private static void Test() {
+		// Get test bytes from text file
+		string[] tests = System.IO.File.ReadAllLines("TrackerService/test_data.txt");
+		// Connect to localhost
+		TcpClient client = new("localhost", 22368);
+		// Get the stream to send data to the server
+		NetworkStream stream = client.GetStream();
+
+		for (int i = 0; i < 10; i++) {
+			stream.Write(tests[i].Split('-').Select(x => Convert.ToByte(x, 16)).ToArray());
+
+			Thread.Sleep(1000);
+		}
+
+		stream.Close();
 	}
 }
