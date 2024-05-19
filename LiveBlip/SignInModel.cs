fcbc0438a1a95db5de872d5c565016c5f6471579
Robert Burston <robbieburston@gmail.com>
2024-05-19 23:44:35 +0200

    Remove null from string

--- LiveBlip/SignInModel.cs
+++ LiveBlip/SignInModel.cs
@@ -3,8 +3,8 @@ namespace LiveBlip.Models
 {
     public class SignInModel
     {
-        public string? Username { get; set; }= string.Empty;
-        public string? Password { get; set; } = string.Empty;
+        public string Username { get; set; }= string.Empty;
+        public string Password { get; set; } = string.Empty;
 
     }
 }
