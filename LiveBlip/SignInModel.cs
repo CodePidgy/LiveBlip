f2d245cf062c8fb13ad1565e60abd243f770514c
Robert Burston <robbieburston@gmail.com>
2024-05-20 11:57:03 +0200

    Add [Required] annotations to variables

--- LiveBlip/SignInModel.cs
+++ LiveBlip/SignInModel.cs
@@ -3,8 +3,10 @@ namespace LiveBlip.Models
 {
     public class SignInModel
     {
-        public string Username { get; set; }= string.Empty;
-        public string Password { get; set; } = string.Empty;
+		[Required]
+        public string Username { get; set; }
+		[Required]
+        public string Password { get; set; }
 
     }
 }
