f2e442f6fad9605773ed9f9827e71616e776e242
Robert Burston <robbieburston@gmail.com>
2024-05-21 23:14:58 +0200

    Add confirm password and email variables

--- LiveBlip/SignUpModel.cs
+++ LiveBlip/SignUpModel.cs
@@ -7,5 +7,10 @@ public class SignUpModel {
 	public string regUname { get; set; }
 	[Required]
 	public string regPword { get; set; }
+	[Required]
+	public string confirmPword { get; set;}
+	[Required]
+	public string Email { get; set; }
+
 
 }
