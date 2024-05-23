455fd48bd1ca67dd482d37081ad40bcb48749019
Robert Burston <robbieburston@gmail.com>
2024-05-23 13:12:13 +0200

    refactor variable name to be shorter
    changed instance tied to it in page code

--- LiveBlip/Models/SignUpModel.cs
+++ LiveBlip/Models/SignUpModel.cs
@@ -4,11 +4,14 @@ namespace LiveBlip.Models;
 
 public class SignUpModel {
 	[Required]
+	[StringLength(8, ErrorMessage = "Name length can't be more than 8.")]
 	public string Username { get; set; }
 	[Required]
+	[StringLength(30, ErrorMessage = "Password must be at least 8 characters long.", MinimumLength = 8)]
 	public string Password { get; set; }
 	[Required]
-	public string confirmPassword { get; set; }
+	[Compare(nameof(Password))]
+	public string Password2 { get; set; }
 	[Required]
 	public string Email { get; set; }
 
