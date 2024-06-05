4aa467b800fca8c560a103db1f238b58d493fa1d
Robert Burston <robbieburston@gmail.com>
2024-06-05 15:25:44 +0200

    Improve UserModel with required fields and limitations
    also added password and confirm password variables

--- LiveBlip/Models/UserModel.cs
+++ LiveBlip/Models/UserModel.cs
@@ -3,10 +3,23 @@ using System.ComponentModel.DataAnnotations;
 namespace LiveBlip.Models;
 	public class UserModel
 		{
+			[Required]
+			[StringLength(8, ErrorMessage = "Name length can't be more than 8.")]
 			public string Username { get; set; }
+			[Required]
 			public string Email { get; set; }
+			[Required]
+			[StringLength(30, ErrorMessage = "Password must be at least 8 characters long.", MinimumLength = 8)]
+			public string Password { get; set; }
+			[Required]
+			[Compare(nameof(Password))]
+			public string Password2 { get; set; }
+			[Required]
 			public string FirstName { get; set; }
+			[Required]
 			public string LastName { get; set; }
+			[Required]
         	public string Location { get; set; }
+
 		}
 
